19f78b2c19f4c75257ff5f6a4283ecdcf1b30721
DiegoVanB <diegovanbelle@outlook.com>
2018-11-29 13:33:39 +0100

    Initial commit

---
+++ Server/src/main/java/com/project/floorball/FloorballApplication.java
@@ -0,0 +1,12 @@
+package com.project.floorball;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FloorballApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FloorballApplication.class, args);
+    }
+}
